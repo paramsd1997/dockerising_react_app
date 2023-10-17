5be10e12cd898b59a075d1115a06c7553b754dde
Parmeshwar * <parmeshwar.1@globallogic.com>
2023-10-17 21:35:39 +0530

    Gen AI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import SearchResults from './SearchResult';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className='search'> Search  </h3>
+      <SearchResults />
+    </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/SearchResult.js
@@ -0,0 +1,67 @@
+// SearchResults.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import './App.css'; // Make sure to import your CSS file
+
+
+
+const SearchResults = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.post(
+        'https://cogsearchforins.search.windows.net/indexes/find/docs/search?api-version=2023-07-01-Preview',
+        {
+          search: query,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': 'KhCytrcE73Lq0sndjHJRjU7H0mxF2gV1FNDPRFt2SSAzSeCPFnQV',
+          },
+        }
+      );
+
+      setResults(response.data.value);
+    } catch (error) {
+      console.error('Error searching:', error);
+    }
+  };
+  const top5results = results.slice(0, 5);
+  return (
+          <div>
+
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+<button className="search-button" onClick={handleSearch}>Search</button>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Score</th>
+            <th>CSR_ID </th>
+            {/* Add more table headers as needed */}
+          </tr>
+        </thead>
+        <tbody>
+        {top5results.map((result)=> (
+          //{results.map((result) => (
+            <tr key={result.Score}>
+             <td>SearchScore: {result['@search.score']}</td>
+              <td>CSR_ID: {result.content.match(/CSR_ID: ([^\n]+)/)[1]}</td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+
+export default SearchResults;
